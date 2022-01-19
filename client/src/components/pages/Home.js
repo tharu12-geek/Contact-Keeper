10113018742a48fb33946128d5789e1d0e12aabc
Tharushika Rathnayake <tharushikamahekshi@gmail.com>
2022-01-19 09:23:51 +0530

    first commit

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect} from 'react';
+import Contacts from '../contacts/Contacts';
+import ContactForm from '../../context/contact/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
+import AuthContext from '../../context/auth/authContext';
+
+const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect( () => {
+    authContext.loadUser();
+  }, []);
+
+  return (
+    <div className='grid-2'>
+      <div><ContactForm /> </div>
+    <div>
+      <ContactFilter />
+      <Contacts />
+    </div>
+    </div>
+  );
+};
+
+export default Home;
